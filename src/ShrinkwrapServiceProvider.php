4370c86f9d1783c58f5c1b672fab4fee69896d7f
Jan-Merijn Versteeg <janmerijnversteg@gmail.com>
2023-03-01 15:31:49 +0100

    Update providers stuff

--- src/ShrinkwrapServiceProvider.php
+++ src/ShrinkwrapServiceProvider.php
@@ -2,15 +2,22 @@
 
 namespace ZEM\Shrinkwrap;
 
+use Illuminate\Support\Collection;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class ShrinkwrapServiceProvider extends PackageServiceProvider
 {
+
     public function configurePackage (Package $package): void
     {
-        $package
-            ->name('shrinkwrap')
-            ->publishesServiceProvider(ShrinkwrapMacrosProvider::class);
+        $package->name('shrinkwrap');
+    }
+
+    public function boot ()
+    {
+        Collection::macro('shrink', function () {
+            return Facility::shrink($this);
+        });
     }
 }
